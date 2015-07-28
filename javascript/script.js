8ddea26ea33d621c223fde359ba1a0e868b63bb4
MarianaLucut <mariana.lucut94@gmail.com>
2015-07-28 16:59:51 +0300

    Animation_responsivePage
    Animation_responsivePage

--- javascript/script.js
+++ javascript/script.js
@@ -18,7 +18,7 @@ var questions = [{
     },{
   question:"4)20-7= ",
   answer:13,
-    topValue:60,
+    topValue:70,
     leftValue:50
     },{
   question:"5)10+8= ",
@@ -48,47 +48,66 @@ var questions = [{
     },{
   question:"10)4*8= ",
   answer:32,
-    topValue:30,
+    topValue:50,
     leftValue:60
-
   }];
+
 var currentQuestion =0;
 var previousQuestion= 0;
 var remainQuestions = questions.length;
 var listOfQuestions= displayImagesAndQuestions();
 var win = window.innerWidth;
 var hin = window.innerHeight;
-var topRabbitValue=60;
-var leftRabbitValue=60;
+var topRabbitValue;
+var leftRabbitValue;
 var ok=0;
+var maximLeft;
+var maximTop;
+
 $('body').append(listOfQuestions);
-// $('.img-responsive').css('width','120px');
+
+topRabbitValue= 25;
+leftRabbitValue= 80;
 setQuestionsPositions();
 displayStartMessage();
 displayImage();
-setRabbitPosition();
+displayPageElements();
 
-$(window).resize(function()
+$(window).on('resize',displayPageElements);
+
+function displayPageElements()
 {
-setQuestionsPositions();
-setRabbitPosition();
-$('.images').css('width',($(window).width()*0.3)+'px');
-// $('#rabbitGame').css('width',($(window).width()*0.3)+'px');
-$('#mesaj').css('font-size',($(window).width()/2*0.02)+'px');
+    if ($(window).width()>1500)
+    {
+        maximLeft = 100;
+        maximTop = -5;
+    }
+    if ($(window).width()<1500 && $(window).width()>800)
+    {
+        maximLeft = 40;
+        maximTop = 10;
+    }
+    if ($(window).width()<800)
+     {
+        maximLeft = 20;
+        maximTop=40;
 
-$('.tableQuestions').css('font-size',($(window).width()/2*0.015)+'px');
-$('.tableQuestions').css('width',($(window).width()*0.1)+'px');
+     }
 
-// $('#imageCarrot').css('height',($(window).height()*10)+'px');
-
-  });
+    setRabbitPosition();
+    setQuestionsPositions();
+    $('.images').css('width',($(window).width()*0.3)+'px');
+    $('#rabbitGame').css('width',($(window).width()/1.5*0.07)+'px');
+    $('#mesaj').css('font-size',($(window).width()/3*0.02)+'px');
+    $('.tableQuestions').css('font-size',($(window).width()/2*0.015)+'px');
+    $('.tableQuestions').css('width',($(window).width()*0.05)+'px');
+ }
 
 function displayImage()
-{
-	var im='<img class="img-responsive" id="rabbitGame" src="images/bunny.png">';
-	 $("#imagesDiv").append(im);
-	 
-}
+  {
+     var im='<img  id="rabbitGame" src="images/bunny.png">';
+  	 $("#imagesDiv").append(im);
+  }
 
 function setRabbitPosition()
 { 
@@ -105,9 +124,9 @@ function setRabbitPosition()
    }
   else
    {
-    var auxiliarVariable = $("#"+currentQuestion).offset();
-     newPos.left=auxiliarVariable.left-5/win*w;
-     newPos.top = auxiliarVariable.top+100/hin*h;
+     var auxiliarVariable = $("#"+currentQuestion).offset();
+     newPos.left=auxiliarVariable.left+maximLeft;
+     newPos.top = auxiliarVariable.top-maximTop;
      $("#rabbitGame").offset(newPos);
    }
 }
@@ -123,16 +142,15 @@ function displayImagesAndQuestions()
      var output="";
      var aux;
      for(var j = 0 ; j< questions.length; j++)
-       { //output+='<div class="question" id="'+(+j)+'"><table ><tr><td><img id="imageCarrot" class="img-responsive" src="images/carrot.png"></td></tr> <tr><table id="t'+j+'" class="tableQuestions"><td>'+questions[j].question+'<input type="text" class="inputStyle" id="input'+j+'" name="answer"></input></td></table></tr></table></div>';
-         output+='<div class="question" id="'+( + j )+'"><div class="images"><img class="img-responsive" type="image" id="imageCarrot" src="images/carrot.png"></div><div id="t'+j+'" class="tableQuestions">'+questions[j].question+'<input class="textBox" type="text" class="inputStyle" id="input'+j+'" name="answer"></input></div></div>';
-        }
+         output+='<div class="question" id="'+( + j )+'"><div class="images"><img class="img-responsive" type="image" id="imageCarrot" src="images/carrot.png"></div><div id="t'+j+'" class="tableQuestions"><div id="nou">'+questions[j].question+'<input class="textBox" type="text" class="inputStyle" id="input'+j+'" name="answer"></input></div></div></div>';
+
      return output;
 }
 
 function setQuestionsPositions()
   { var w = window.innerWidth;
-var h = window.innerHeight;
-     for(var j = 0; j<questions.length; j++)
+    var h = window.innerHeight;
+    for(var j = 0; j<questions.length; j++)
         {
           newPos = new Object();
           newPos.left = questions[j].leftValue*w/100;
@@ -149,10 +167,11 @@ $(".question").on('click',function(e)
        $("#mesaj").html("");
        previousQuestion = currentQuestion;
        currentQuestion = $(this).attr('id');
-       var topValue  = questions[currentQuestion].topValue*h/100+50;
-       var leftValue = questions[currentQuestion].leftValue*w/100+100;
-        
-      
+       var auxiliarVariable = $("#"+currentQuestion).offset();
+       newPos= new Object();
+       newPos.left=auxiliarVariable.left+maximLeft;
+       newPos.top = auxiliarVariable.top-maximTop;
+
        $("#t"+currentQuestion).addClass("currentQuestion");
        var isVisible = $('#' + previousQuestion).is(':visible');
 
@@ -161,9 +180,10 @@ $(".question").on('click',function(e)
 
        if (isVisible == true && currentQuestion != previousQuestion)
                 $("#t" + previousQuestion).removeClass("currentQuestion");
-      
+
        $('#rabbitGame').animate({
-            top: topValue,left:leftValue},300);
+            top: newPos.top,left:newPos.left},250);
+
   });
 
 
@@ -175,9 +195,9 @@ $(".question").keydown(function (e)
          var answer = questions[idQuestion].answer;
          var userAnswer = $("#input"+idQuestion).val();
 
-        if (answer == userAnswer)
+         if (answer == userAnswer)
              {  $("#mesaj").addClass("success");
-                $("#mesaj").html("   Iepurasul a mai mancat inca un morcov!!").show().delay(2000).fadeOut();
+                $("#mesaj").html("   Iepurasul a mancat inca un morcov!!").show().delay(2000).fadeOut();
                 $(this).css("visibility", "hidden");
                 remainQuestions--;
                 if(remainQuestions == 0)
@@ -185,12 +205,14 @@ $(".question").keydown(function (e)
                        $("#mesaj").removeClass("success").removeClass("error").addClass("warning");
                        $("#mesaj").html("Iepurasul a mancat tot si pleaca la somn!!"+ "&#9786").css({"position": "relative", "top": "500px", "left": "500px"}).show().delay(3000).fadeOut();
                        $("#rabbit").css("visibility", "hidden");
-                       $("#imageSource").css("visibility", "hidden"); 
+                       $("#rabbitGame").css("visibility", "hidden"); 
+                       var output ='<button type="button"><a href = "../mariana_animation/index.html">Reincepe joc!</button>';
+                       $('body').append(output);
                     }
                 }
                 else
                 {
-                  $("#mesaj").removeClass("success").addClass("error");
+                   $("#mesaj").removeClass("success").addClass("error");
                    $("#mesaj").html("Raspunsul nu este corect!Mai incearca"+"&#9785").show().delay(3000).fadeOut();
                    $('#input'+idQuestion).val('');
                 }
